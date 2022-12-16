58f8f743c719829f106210e7581320254d4435e0
utzuro <oleh.skotar@kyash.co>
2022-12-16 19:02:11 +0900

    add installation instructinons

--- packages/mac.sh
+++ packages/mac.sh
@@ -1,7 +1,16 @@
-#!usr/bin/env bash
-
+#!/usr/bin/env bash
 # Install homebrew and use it to install all the tools.
 
 # General: activate in any case.
+xcode-select --install 
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# shell tools
+brew install curl wget zsh git tmux vim docker jq peco ranger zplug
+# pdf
+brew tap zegervdv/zathura
+brew install zathura zathura-pdf-mupdf
 
 # Extra: can be skipped safely.
+brew tap versent/homebrew-taps
+brew install tfenv goenv saml2aws
+brew install ledger
--- packages/shell_install.sh
+++ packages/shell_install.sh
@@ -3,5 +3,9 @@
 # Use wget or curl to install OS agnostic tools.
 
 # General: activate in any case.
+# Requires curl, git, zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Extra: can be skipped safely.
