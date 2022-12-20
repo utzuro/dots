a0ca89de76eb470eec5c934f86bb231187b21a7e
utzuro <oleh.skotar@kyash.co>
2022-12-20 19:07:53 +0900

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -n "$alchemy" ]; then alchemy="$HOME/alchemy"; fi
-if [ -n "$manuscripts" ]; then manuscripts="$HOME/magic/manuscripts"; fi
+alchemy="$HOME/alchemy"
 
 # Reliable way to get full path
 DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
@@ -11,23 +10,23 @@ cd "$DIR" || exit
 
 # Install packages
 printf "⌛... Installing missing packages... 📦☄\n"
-#if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-#    "$DIR"/packages/archlinux.sh
-#elif [[ "$OSTYPE" == "darwin"* ]]; then
-#    "$DIR"/packages/mac.sh
-#elif [[ "$OSTYPE" == "linux-android"* ]]; then
-#    "$DIR"/packages/termux.sh
-#elif [[ "$OSTYPE" == "cygwin"* ]]; then
-#    "$DIR"/packages/cygwin.sh
-#elif [[ "$OSTYPE" == "msys"* ]]; then
-#    "$DIR"/packages/ms.sh
-#else
-#    printf " ¯ \ _ (ツ) _ / ¯  Unknown system, packages won't be installed.\n"
-#fi
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    "$DIR"/packages/archlinux.sh
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    "$DIR"/packages/mac.sh
+elif [[ "$OSTYPE" == "linux-android"* ]]; then
+    "$DIR"/packages/termux.sh
+elif [[ "$OSTYPE" == "cygwin"* ]]; then
+    "$DIR"/packages/cygwin.sh
+elif [[ "$OSTYPE" == "msys"* ]]; then
+    "$DIR"/packages/ms.sh
+else
+    printf " ¯ \ _ (ツ) _ / ¯  Unknown system, packages won't be installed.\n"
+fi
 
 printf "\n⌛... Creating default folders... 📂\n"
-mkdir -p "$alchemy"/ingredients "$HOME"/magic/ingredients
-if [ -d "$alchemy"/scripts ]; then
+mkdir -p "${alchemy:?}"/ingredients "$HOME"/magic/ingredients
+if ! [ -d "$alchemy"/scripts ]; then
   git clone https://gitlab.com/utzuro/scripts.git "$alchemy"/scripts
   cd "$alchemy"/scripts || exit
   git remote remove origin
@@ -36,31 +35,37 @@ if [ -d "$alchemy"/scripts ]; then
 fi
 
 # Install all the OS agnostic shell tools
-#"$DIR"/packages/shell_install.sh
+"$DIR"/packages/shell_install.sh
 
 printf "\n⌛... Linking configuration files to the corresponding places in the system... 🖇\n"
 # Vim
-ln -sfv "$DIR"/config/vim/.vimrc ~/
-ln -sfv "$DIR"/config/vim/.vim/*.vim ~/.vim/
-mkdir -p ~/.vim/after/syntax
-ln -sfv "$DIR"/config/vim/.vim/after/syntax/asciidoc.vim ~/.vim/after/syntax/
+ln -sfv "$DIR"/config/vim/.vimrc "$HOME"/
+ln -sfv "$DIR"/config/vim/.ideavimrc "$HOME"/
+ln -sfv "$DIR"/config/vim/.vim/*.vim "$HOME"/.vim/
+mkdir -p "$HOME"/.vim/after/syntax
+ln -sfv "$DIR"/config/vim/.vim/after/syntax/asciidoc.vim "$HOME"/.vim/after/syntax/
 vim +PluginInstall +qall
 
 # Tmux
-ln -sfv "$DIR"/config/tmux/.tmux.conf ~/
+ln -sfv "$DIR"/config/tmux/.tmux.conf "$HOME"/
 
 # Shell
-ln -sfv "$DIR"/config/zsh/.zshrc ~/
-ln -sfv "$DIR"/config/zsh/.p10k.zsh ~/
+ln -sfv "$DIR"/config/zsh/.zshrc "$HOME"/
+ln -sfv "$DIR"/config/zsh/.p10k.zsh "$HOME"/
 
 # Window manager
 if xhost >& /dev/null ; then 
     printf "🧿 Detected Xorg, configuring...\n"
-    ln -sfv "$DIR"/config/xorg/.Xresources ~/
-    ln -sfv "$DIR"/config/xorg/.xinitrc ~/
+    ln -sfv "$DIR"/config/xorg/.Xresources "$HOME"/
+    ln -sfv "$DIR"/config/xorg/.xinitrc "$HOME"/
+    ln -sfv "$DIR"/config/dunst "$HOME"/.config/
+    ln -sfv "$DIR"/config/rasi "$HOME"/.config/
+    ln -sfv "$DIR"/config/rofi "$HOME"/.config/
+    ln -sfv "$DIR"/config/mpd "$HOME"/.config/
+    ln -sfv "$DIR"/config/qt5ct "$HOME"/.config/
 fi
 
 if [ -d "$HOME/.config/i3" ]; then
     printf "🧿 Detected i3, configuring...\n"
-    ln -sfv "$DIR"/config/i3/config ~/.config/i3/
+    ln -sfv "$DIR"/config/i3/config "$HOME"/.config/i3/
 fi
--- packages/termux.sh
+++ packages/termux.sh
@@ -1,1 +1,1 @@
-pkg install vim-python git tmux zsh qalc bc python golang nodejs perl ruby build-essentials cmake ncurses-utils curl tree
+pkg install vim-python git tmux zsh qalc bc python golang nodejs perl ruby rust cargo build-essentials cmake ncurses-utils curl tree
