ae5dfb5a04908654ffd7f36c70ac68171ce50c21
utzuro <utzuro@protonmail.com>
2023-07-02 18:41:36 +0900

    config windows auto-install

--- packages/win.sh
+++ packages/win.sh
@@ -1,11 +1,28 @@
 # WIP
-sh <(curl -L https://nixos.org/nix/install) --no-daemon
+# sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
 # Install scoop as package manager (powershell req)
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-irm get.scoop.sh | iex
+# Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+# irm get.scoop.sh | iex
 
-scoop bucket add extra versions
-scoop install ffmpeg vim 7zip curl wget sudo git openssh coreutils grep sed less
-scoop install python ruby go nodejs
-scoop instal neovim vscode docker docker-compose yt-dlp
+scoop bucket add extras 
+# tools
+scoop install ffmpeg vim 7zip curl wget sudo openssh coreutils yt-dlp
+# utils
+scoop install autohotkey powertoys
+# apps
+scoop install firefox libreoffice qbittorrent vlc sumatrapdf anki eartrumpet 
+scoop install discord signal forkgram 
+# dev
+scoop install python go nodejs rustup 
+scoop install neovim jetbrains-toolbox vscode docker docker-compose 
+# games
+scoop bucket add versions
+scoop install steam
+
+# run with admin rights (buckets can be installed as normal user)
+scoop bucket add games
+scoop bucket add nonportable 
+# scoop install epic-games-launcher
+# scoop install protonvpn-np voicemeeter-np 
+
