6bece7f1344d0e5feda93a914992092d0cabd55e
utzuro <utzuro@pm.me>
2024-04-14 18:35:54 +0900

    define template for home-manger config

--- config/nix/home.nix
+++ config/nix/home.nix
@@ -1,2 +1,22 @@
 # put home-manager configs here when needed
 # install with `home-manager switch --flake .#void`
+
+
+{ config, pkgs, ... }:
+
+{
+  home.username = "void";
+  home.homeDirectory = "/home/void";
+
+  home.stateVersion = "23.11";
+
+  home.packages = [
+
+  ]
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.home-manager.enable = true;
+}
