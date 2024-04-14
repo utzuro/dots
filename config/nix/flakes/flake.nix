e504ea6b2e0934a5ca6f9c40db73ca783b21e26f
utzuro <utzuro@pm.me>
2024-04-14 18:21:01 +0900

    add home-manager to the flake

--- config/nix/flakes/flake.nix
+++ config/nix/flakes/flake.nix
@@ -3,16 +3,26 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }: 
+  outputs = { self, nixpkgs, home-manager, ... }: 
   let
     lib = nixpkgs.lib;
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations = {
       void = lib.nixosSystem {
-        system = "x86_64-linux"; 
-        modules = [ ../configuration.nix ];
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      void = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
       };
     };
   };
