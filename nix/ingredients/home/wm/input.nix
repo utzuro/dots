1883e4a70e2882ed2dd1ab83ac8c75b740d19f81
void <utzuro@pm.me>
2024-05-23 21:42:16 +0900

    manage fcitx profile with nix

--- nix/ingredients/home/wm/input.nix
+++ nix/ingredients/home/wm/input.nix
@@ -7,58 +7,74 @@
       fcitx5-mozc
       fcitx5-gtk
     ]; 
-    #fcitx5.waylandFrontend = true;
+    fcitx5.waylandFrontend = true;
   };
+  home.file.".config/fcitx5/profile".text = ''
+    [Groups/0]
+    Name=Default
+    Default Layout=us
+    DefaultIM=mozc
+
+    [Groups/0/Items/0]
+    Name=keyboard-us
+    Layout=
+
+    [Groups/0/Items/1]
+    Name=mozc
+    Layout=
+
+    [GroupOrder]
+    0=Default
+    '';
   home.file.".config/fcitx5/config".text = ''
+    [Hotkey]
+    EnumerateWithTriggerKeys=True
+    AltTriggerKeys=
+    EnumerateForwardKeys=
+    EnumerateBackwardKeys=
+    EnumerateSkipFirst=False
+    EnumerateGroupForwardKeys=
+    EnumerateGroupBackwardKeys=
+    TogglePreedit=
 
-  [Hotkey]
-  EnumerateWithTriggerKeys=True
-  AltTriggerKeys=
-  EnumerateForwardKeys=
-  EnumerateBackwardKeys=
-  EnumerateSkipFirst=False
-  EnumerateGroupForwardKeys=
-  EnumerateGroupBackwardKeys=
-  TogglePreedit=
+    [Hotkey/TriggerKeys]
+    0=Super+comma
 
-  [Hotkey/TriggerKeys]
-  0=Super+comma
+    [Hotkey/ActivateKeys]
+    0=F2
 
-  [Hotkey/ActivateKeys]
-  0=F2
+    [Hotkey/DeactivateKeys]
+    0=F1
 
-  [Hotkey/DeactivateKeys]
-  0=F1
+    [Hotkey/PrevPage]
+    0=Up
 
-  [Hotkey/PrevPage]
-  0=Up
+    [Hotkey/NextPage]
+    0=Down
 
-  [Hotkey/NextPage]
-  0=Down
+    [Hotkey/PrevCandidate]
+    0=Shift+Tab
 
-  [Hotkey/PrevCandidate]
-  0=Shift+Tab
+    [Hotkey/NextCandidate]
+    0=Tab
 
-  [Hotkey/NextCandidate]
-  0=Tab
-
-  [Behavior]
-  ActiveByDefault=False
-  ShareInputState=No
-  PreeditEnabledByDefault=False
-  ShowInputMethodInformation=False
-  showInputMethodInformationWhenFocusIn=False
-  CompactInputMethodInformation=False
-  ShowFirstInputMethodInformation=False
-  DefaultPageSize=5
-  OverrideXkbOption=False
-  CustomXkbOption=
-  EnabledAddons=
-  DisabledAddons=
-  PreloadInputMethod=True
-  AllowInputMethodForPassword=False
-  ShowPreeditForPassword=False
-  AutoSavePeriod=30
-
+    [Behavior]
+    ActiveByDefault=False
+    resetStateWhenFocusIn=No
+    ShareInputState=No
+    PreeditEnabledByDefault=True
+    ShowInputMethodInformation=False
+    showInputMethodInformationWhenFocusIn=False
+    CompactInputMethodInformation=False
+    ShowFirstInputMethodInformation=False
+    DefaultPageSize=5
+    OverrideXkbOption=False
+    CustomXkbOption=
+    EnabledAddons=
+    DisabledAddons=
+    PreloadInputMethod=True
+    AllowInputMethodForPassword=False
+    ShowPreeditForPassword=False
+    AutoSavePeriod=30
   '';
 }
