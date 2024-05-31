61f6aac9295a1afbb530df6e8c2151dea9813f94
void <utzuro@pm.me>
2024-05-31 15:02:37 +0900

    update ncmpcpp binding

--- nix/ingredients/home/media.nix
+++ nix/ingredients/home/media.nix
@@ -24,6 +24,29 @@
     tuir
   ];
 
+  home.file.".config/ncmpcpp/config".text = ''
+    def_key "l"
+      next_column
+    def_key "h"
+      previous_column
+    def_key "k"
+      scroll_up
+    def_key "j"
+      scroll_down
+    def_key "shift-k"
+      select_item
+      scroll_up
+    def_key "shift-j"
+      select_item
+      scroll_down
+    def_key "G"
+      page_down
+    def_key "g"
+      page_up
+    def_key "%"
+      scroll_down_album
+    '';
+
   xdg.mimeApps.defaultApplications = {
     "text/plain" = [ "neovide.desktop" ];
     "application/pdf" = [ "zathura.desktop" ];
