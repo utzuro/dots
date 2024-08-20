d09ff37618075aa6f42c2d78267e9b58b8ed9877
void <utzuro@pm.me>
2024-08-20 09:35:59 +0900

    update && git push

--- nix/modules/laptop.nix
+++ nix/modules/laptop.nix
@@ -1,47 +1,9 @@
-# install and update with 
-# `sudo nixos-rebuild switch --flake .#system --impure`
-# on first install enable unstable channel
-# `nix-channel --add https://nixos.org/channels/nixos-unstable nixos`
-
-{ config, lib, pkgs, user, system, inputs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
     [ 
-      ../${system.host}/hardware-configuration.nix
-
-      ( import ./system/basic.nix {
-        inherit config pkgs user;
-      })
-
-      ( import ./system/network.nix {
-        inherit config pkgs user system;
-      })
-
-      ( import ./system/system.nix {
-        inherit config pkgs user system;
-      })
-
-      ./system/comms.nix
-      # ./system/vpn.nix
-      ./system/security.nix
-      ./system/power-laptop.nix
-      ./system/video.nix
-      # ./system/autorandr.nix
-
-      ./system/wm/shared.nix
-      ./system/wm/i3.nix
-      ./system/wm/hyprland.nix
-      # ./system/wm/kde.nix
-
+      ./system/power-laptop.nix.nix
       ./system/gaming.nix
-
-      ( import ./system/virtualization.nix {
-        storageDriver = "btrfs"; inherit pkgs user lib;
-      })
-
     ];
-
-  system.copySystemConfiguration = false;
-  system.stateVersion = "23.11";
 }
