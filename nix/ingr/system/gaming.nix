1e98ca54e86df12ce1cd0aca92f4e70734566c35
void <utzuro@pm.me>
2024-09-04 13:24:46 +0900

    add steam instructions

--- nix/ingr/system/gaming.nix
+++ nix/ingr/system/gaming.nix
@@ -7,6 +7,14 @@
   services.jack.alsa.support32Bit = true;
   services.pipewire.alsa.support32Bit = true;
 
+  # Prepend Launch options for Steam games with:
+  # - gamemode %command% too run optimizations
+  # - mangohud %command% to see FPS and other info
+  # - gamescope %command% to run in container
+  # Or combine with
+  # mangohud gamemoderun %command% OR MANGOHUD=1 gamemoderun %command%
+  # Specify container parameters 
+  # gamescope -W 3840 -H 2160 -r 119 -f -e -- mangohud gamemoderun %command%
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
@@ -19,22 +27,16 @@
   environment.systemPackages = with pkgs; [ steam vulkan-headers ntfs3g protonup vcmi ]; 
   environment.sessionVariables = { 
     STEAM_EXTRA_COMPAT_TOOLS_PATHS =
-      "\${HOME}/.steam/root/compatibilitytools.d";
+      "\${HOME}/.steam/root/compatibilitytools.d"; # for protonup to work
   };
 
   nixpkgs.config.packageOverrides = pkgs: {
     steam = pkgs.steam.override {
       extraPkgs = pkgs: with pkgs; [
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXScrnSaver
-        libpng
-        libpulseaudio
-        libvorbis
-        stdenv.cc.cc.lib
-        libkrb5
-        keyutils
+        mangohud
+        xorg.libXcursor xorg.libXi xorg.libXinerama xorg.libXScrnSaver
+        libpng libpulseaudio libvorbis
+        stdenv.cc.cc.lib libkrb5 keyutils
       ];
     };
   };
