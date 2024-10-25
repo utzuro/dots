d980d7b912a13d4ac176f151420b75ec9a0d3614
void <utzuro@pm.me>
2024-10-25 11:43:10 +0900

    use correct encrypted idsk uuid on x240

--- nix/machines/x240/hardware-configuration.nix
+++ nix/machines/x240/hardware-configuration.nix
@@ -7,7 +7,7 @@
 
   boot.initrd.luks.devices = {
     nixenc = {
-      device = "/dev/disk/by-uuid/???";
+      device = "/dev/disk/by-uuid/bda8035d-0cd2-4f97-ad26-eb124067ea6a";
       preLVM = true;
       allowDiscards = true;
     };
