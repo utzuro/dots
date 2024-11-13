73cdb84a9eae48926922a0220e466bd9aceb71e8
void <utzuro@pm.me>
2024-11-14 08:43:58 +0900

    clean up comms servers for now

--- nix/ingr/system/comms.nix
+++ nix/ingr/system/comms.nix
@@ -1,37 +1,5 @@
 { pkgs, ...}:
 
 {
-  nixpkgs.config.bitlbee.enableLibPurple = true;
-
-  environment.systemPackages = with pkgs; [
-    thelounge weechat
-  ];
-
-  services = [
-    matrix-conduit = {
-      enable = true;
-      settings.global = {
-        allow_registration = false; # make true to create user
-        address = "::1";
-        database_backend = "rocksdb";
-# For VoIP see https://docs.conduit.rs/turn.html, and https://github.com/element-hq/synapse/blob/develop/docs/turn-howto.md for more details
-      # turn_uris = [
-      #  "turn:your.turn.url?transport=udp"
-      #  "turn:your.turn.url?transport=tcp"
-      # ];
-      # turn_secret = "your secret";
-    bitlbee = {
-      enable = true;
-      plugins = with pkgs; [
-        bitlbee-discord
-        bitlbee-facebook
-        bitlbee-steam
-      ];
-      libpurple_plugins = with pkgs; [
-        purple-signald
-        purple-slack
-        purple-matrix
-      ];
-    };
-  ]
+  environment.systemPackages = with pkgs; [ ];
 }
