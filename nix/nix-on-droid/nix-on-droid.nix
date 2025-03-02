a64a22a5a4d68dc28c924dfd8a7ff5f6454d232f
nix-on-droid <utzuro@pm.me>
2025-03-02 18:06:10 +0900

    add basic android configs

---
+++ nix/nix-on-droid/nix-on-droid.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }:
+
+{
+  environment.packages = with pkgs; [
+    vim git tmux openssh zsh
+    busybox
+    zip unzip
+  ];
+
+  environment.etcBackupExtension = ".bak";
+  system.stateVersion = "24.05";
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  time.timeZone = "Asia/Tokyo";
+}
