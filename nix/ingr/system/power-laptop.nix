5f86f7a32bbd207ac2e3ca78c0d8ba83a92ea500
void <utzuro@pm.me>
2025-05-08 14:55:53 +0900

    update

--- nix/ingr/system/power-laptop.nix
+++ nix/ingr/system/power-laptop.nix
@@ -5,16 +5,16 @@
     kernelModules = [ "i2c-dev" "i2c-piix4" "cpufreq_powersave" ];
   };
 
-  # powerManagement.enable = true; # managed by tlp
+  powerManagement.enable = true; # managed by tlp
   services.thermald.enable = true;
-  services.tlp.enable = true;
+  # services.tlp.enable = true;
 
   environment = {
     systemPackages = with pkgs; [
       cpufrequtils 
       cpupower-gui
       powerstat
-      tlp
+      # tlp
     ];
   };
 }
