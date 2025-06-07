b2540273fda34c392ded9dac26b9f29e5954af20
void <utzuro@pm.me>
2025-06-07 14:50:46 +0900

    update

--- config/hypr/wall.sh
+++ config/hypr/wall.sh
@@ -2,16 +2,17 @@
 
 swww-daemon &> /dev/null &!
 
-EXTERNAL_DISK="/mnt/seance"
-EXTERNAL_ALT="/mnt/zalot"
-IMAGE_DIR="mysticism/i/walls"
+EXTERNAL_DISK="/mnt/seance/mysticism"
+EXTERNAL_ALT="/mnt/zalot/mysticism"
+HOME_DIR="$HOME/mysticism"
+IMAGE_DIR="i/walls"
 
 if [ -d "$EXTERNAL_DISK" ]; then
     WALLS_DIR="$EXTERNAL_DISK/$IMAGE_DIR"
 elif [ -d "$EXTERNAL_ALT" ]; then
     WALLS_DIR="$EXTERNAL_ALT/$IMAGE_DIR"
-elif [ -d "$HOME/$IMAGE_DIR" ]; then
-    WALLS_DIR="$HOME/$IMAGE_DIR"
+elif [ -d "$HOME_DIR/$IMAGE_DIR" ]; then
+    WALLS_DIR="$HOME_DIR/$IMAGE_DIR"
 else
     echo "No valid image directory found. Please check your external disks or home directory."
     exit 1
@@ -25,5 +26,5 @@ fi
 while true; do
     WALL=$(find "$WALLS_DIR" -type f | shuf -n 1)
     swww img "$WALL" --transition-step 180 --transition-fps 60 --transition-type center
-    sleep 10s
+    sleep 1m
 done
