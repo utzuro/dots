4e7a1862cb4e1ceb637227c4446f4553cd56b19b
void <utzuro@pm.me>
2025-06-17 11:57:59 +0900

    update

--- nix/ingr/system/system.nix
+++ nix/ingr/system/system.nix
@@ -35,6 +35,10 @@
   programs.nix-ld = {
     enable = true;
     package = pkgs.nix-ld-rs; 
+    ## If needed, you can add missing libraries here. nix-index-database is your friend to
+    ## find the name of the package from the error message:
+    ## https://github.com/nix-community/nix-index-database
+    libraries = options.programs.ni
   };
 
   nix.gc = {
