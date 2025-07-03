44035973f991484e075a3f3e840b07cc65179fc8
void <utzuro@pm.me>
2025-07-03 16:35:10 +0900

    update

--- nix/wsl/hardware-configuration.nix
+++ nix/wsl/hardware-configuration.nix
@@ -1,5 +1,8 @@
 { config, lib, pkgs, modulesPath, ... }:
 {
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
   # hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
