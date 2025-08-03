2fa5d5c3e46f608b2d12e3ed629cef1cb2c792d4
utzuro <utzuro@pm.me>
2025-08-03 10:32:07 +0900

    setup gaming server

---
+++ nix/ingr/system/game-server.nix
@@ -0,0 +1,48 @@
+{ pkgs, ...}:
+
+{
+
+  services= {
+  # TODO: Serving games ideas:
+  # Homm
+  # WoW / other mmo?
+  # Arma / Squad
+  # Minecraft / Factorio / Terraria
+  # Quake, Doom, UT, Xonotic
+  # OpenRA
+  # 0 A.D. 
+
+  # Voice chat server
+
+  # Streaming games
+  sunshine = {
+    enable = true;
+    capSysAdmin = true;
+    openFirewall = true;
+    settings = {
+      port = 47989;
+      sunshine_name = "void";
+    };
+    applications = {
+      # Example, change later
+      env = {
+        PATH = "$(PATH):$(HOME)/.local/bin";
+      };
+      apps = [
+        {
+          name = "1440p Desktop";
+          prep-cmd = [
+            {
+              do = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-4.mode.2560x1440@144";
+              undo = "${pkgs.kdePackages.libkscreen}/bin/kscreen-doctor output.DP-4.mode.3440x1440@144";
+            }
+          ];
+          exclude-global-prep-cmd = "false";
+          auto-detach = "true";
+        }
+      ];
+    };
+  };
+  };
+
+}
--- nix/ingr/system/gaming.nix
+++ nix/ingr/system/gaming.nix
@@ -3,6 +3,7 @@
 {
   imports = [
     ./steam.nix
+    ./game-server.nix
     # ./minecraft.nix
   ];
 
