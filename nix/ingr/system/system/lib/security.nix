4b2c56a382a064775faee029c1a0b2cc8272137c
utzuro <utzuro@pm.me>
2025-08-11 21:25:18 +0900

    get rid of doas, because it stopped working

--- nix/ingr/system/system/lib/security.nix
+++ nix/ingr/system/system/lib/security.nix
@@ -21,37 +21,38 @@ in
   security = {
     polkit.enable = true;
 
-    doas.enable = true;
-    doas.extraRules = [{
-      users = [ "void" ];
-      keepEnv = true;
-      persist = true;
-    }];
+    # doas stopped working
+    # doas.enable = true;
+    # doas.extraRules = [{
+    #   users = [ "void" ];
+    #   keepEnv = true;
+    #   persist = true;
+    # }];
 
     sudo = {
-      enable = false;
-      # extraRules = [{
-      #   commands = [
-      #     {
-      #       command = "${pkgs.systemd}/bin/systemctl suspend";
-      #       options = [ "NOPASSWD" ];
-      #     }
-      #     {
-      #       command = "${pkgs.systemd}/bin/reboot";
-      #       options = [ "NOPASSWD" ];
-      #     }
-      #     {
-      #       command = "${pkgs.systemd}/bin/poweroff";
-      #       options = [ "NOPASSWD" ];
-      #     }
-      #   ];
-      #   groups = [ "wheel" ];
-      # }];
-      # extraConfig = with pkgs; ''
-      #   Defaults:picloud secure_path="${lib.makeBinPath [
-      #     systemd
-      #   ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-      # '';
+      enable = true;
+      extraRules = [{
+        commands = [
+          {
+            command = "${pkgs.systemd}/bin/systemctl suspend";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/reboot";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/poweroff";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+        groups = [ "wheel" ];
+      }];
+      extraConfig = with pkgs; ''
+        Defaults:picloud secure_path="${lib.makeBinPath [
+          systemd
+        ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+      '';
     };
 
 
@@ -60,7 +61,7 @@ in
   };
 
   environment.systemPackages = with pkgs; [
-    (pkgs.writeScriptBin "sudo" ''exec doas "$@"'')
+    # (pkgs.writeScriptBin "sudo" ''exec doas "$@"'')
     firejail
     clamav
     opensnitch-ui
