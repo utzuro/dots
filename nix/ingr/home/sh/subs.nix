5225f1099f1f6cc6b414f5caca785caed2f0b88f
utzuro <utzuro@pm.me>
2025-08-29 17:55:53 +0900

    add subs

--- nix/ingr/home/sh/subs.nix
+++ nix/ingr/home/sh/subs.nix
@@ -9,33 +9,107 @@
     '';
     urls = [
       # general
-      "https://news.ycombinator.com/rss"
-      "https://xkcd.com/rss.xml"
+      {
+        tags = [ "general" ];
+        url = "https://news.ycombinator.com/rss";
+      }
+      {
+        tags = [ "general" ];
+        url = "https://xkcd.com/rss.xml";
+      }
+      {
+        tags = [ "general" ];
+        url = "http://syndication.thedailywtf.com/TheDailyWtf";
+      }
 
       # tech
-      "https://www.reddit.com/r/programming/.rss"
-      "https://hnrss.org/frontpage"
-      "https://hackaday.com/feed/"
-      "http://feeds.arstechnica.com/arstechnica/technology-lab"
-      "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml"
-      "https://www.reddit.com/r/linux/.rss"
-      "https://lobste.rs/rss"
+      {
+        tags = [ "tech" ];
+        url = "https://www.reddit.com/r/programming/.rss";
+      }
+      {
+        tags = [ "tech" ];
+        url = "https://hnrss.org/frontpage";
+      }
+      {
+        tags = [ "tech" ];
+        url = "https://hackaday.com/feed/";
+      }
+      {
+        tags = [ "tech" ];
+        url = "http://feeds.arstechnica.com/arstechnica/technology-lab";
+      }
+      {
+        tags = [ "tech" ];
+        url = "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml";
+      }
+      {
+        tags = [ "tech" ];
+        url = "https://www.reddit.com/r/linux/.rss";
+      }
+      {
+        tags = [ "tech" ];
+        url = "https://lobste.rs/rss";
+      }
+      {
+        tags = [ "hacking" ];
+        url = "https://www.hackthebox.com/rss/blog/red-teaming"; # or all-content
+      }
+      {
+        tags = [ "hacking" ];
+        url = "https://hackerpublicradio.org/rss"; # hypothetical feed
+      }
+
+      {
+        tags = [ "hacking" ];
+        url = "https://hackaday.com/feed/";
+      }
 
       # dev
-      "https://cprss.s3.amazonaws.com/golangweekly.com.xml"
-      "https://blog.rust-lang.org/feed.xml"
-      "https://lists.yoctoproject.org/g/yocto/rss"
+      {
+        tags = [ "dev" ];
+        url = "https://cprss.s3.amazonaws.com/golangweekly.com.xml";
+      }
+      {
+        tags = [ "dev" ];
+        url = "https://blog.rust-lang.org/feed.xml";
+      }
+      {
+        tags = [ "dev" ];
+        url = "https://lists.yoctoproject.org/g/yocto/rss";
+      }
 
       # sci
-      "http://queue.acm.org/rss/feeds/queuecontent.xml"
-      "https://blog.acolyer.org/feed"
-      "https://arxiv.org/rss/cs"
-      "https://www.3quarksdaily.com/feed"
-      "https://philosophynow.org/rss"
-      "http://www.lightspeedmagazine.com/feed/"
+      {
+        tags = [ "sci" ];
+        url = "http://queue.acm.org/rss/feeds/queuecontent.xml";
+      }
+      {
+        tags = [ "sci" ];
+        url = "https://arxiv.org/rss/cs";
+      }
+      {
+        tags = [ "sci" ];
+        url = "https://www.3quarksdaily.com/feed";
+      }
+      {
+        tags = [ "sci" ];
+        url = "https://philosophynow.org/rss";
+      }
+      {
+        tags = [ "philosophy" ];
+        url = "https://dailynous.com/feed/";
+      }
+      {
+        tags = [ "philosophy" ];
+        url = "https://www.3quarksdaily.com/feed";
+      }
 
       # keep an eye on
-      "https://github.com/nixos/nixpkgs/releases.atom"
+      {
+        tags = [ "watch" ];
+        url = "https://github.com/nixos/nixpkgs/releases.atom";
+      }
     ];
   };
 }
