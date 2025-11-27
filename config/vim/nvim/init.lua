880f96ab3890cf271fa6154b3a572e04e0df99ed
utzuro <utzuro@pm.me>
2025-11-28 08:16:26 +0900

    use lua to init nvim

---
+++ config/vim/nvim/init.lua
@@ -0,0 +1,23 @@
+local function source(file)
+  vim.cmd('source ' .. file)
+end
+
+local files = {
+  '~/.vim/basic.vim',
+  '~/.vim/maps.vim',
+  '~/.vim/auto.vim',
+  '~/.vim/plugins.vim',
+  '~/.vim/custom.vim',
+}
+
+for _, file in ipairs(files) do
+  source(file)
+end
+
+vim.cmd.colorscheme('dracula')
+
+local config_root = vim.fn.stdpath('config')
+local devcontainer_file = config_root .. '/devcontainer.lua'
+if vim.fn.filereadable(devcontainer_file) == 1 then
+  dofile(devcontainer_file)
+end
